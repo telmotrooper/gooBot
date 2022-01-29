b3dc2c5311e630ce5097a9093e44c84bc7179ff3
Telmo Valverde <telmo.valverde@tarmac.io>
2022-01-29 11:57:12 -0300

    Grabbing screen size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from save_image import save_image
 
+
 def main():
     print("gooBot")
     save_image()
--- save_image.py
+++ save_image.py
@@ -4,5 +4,12 @@ from time import time
 
 def save_image():
     # (0,0) is top-left
-    image = ImageGrab.grab()
+    initial_pos = {"x": 0, "y": 1080}
+    screen_size = {"width": 1920, "height": 1080}
+    box = (
+        initial_pos["x"],
+        initial_pos["y"],
+        initial_pos["x"] + screen_size["width"],
+        initial_pos["y"] + screen_size["height"])
+    image = ImageGrab.grab(box)
     image.save(f"/tmp/goo_{int(time())}.png", "PNG")
